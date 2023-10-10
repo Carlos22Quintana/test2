9b3c293795e199053811be12b66b3a95452c5186
Carlos22Quintana <145041995+Carlos22Quintana@users.noreply.github.com>
2023-10-10 18:56:28 +0200

    Martes 10.10 ISM primer apartado

--- ism/test/testISM.py
+++ ism/test/testISM.py
@@ -1,6 +1,9 @@
 #4.1.5. Pass/fail criteria
+import pandas as pd
+
 from common.io.writeToa import readToa
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from config import globalConfig
 from config.l1bConfig import l1bConfig
@@ -20,3 +23,28 @@ for band in bands:
 
     # 2. Read your outputs
     my_toa = readToa(my_toa_path, ism_toa_isrf + band + '.nc')
+
+    # 3. Compare
+    result = my_toa - luss_toa
+    porcentaje = result / my_toa * 100
+    df = pd.DataFrame(porcentaje)
+    porcentaje_df = df.fillna(0) # The division per zero gives some annoying NaN values
+    boolean_comparison = np.array(porcentaje_df < 0.01)
+
+    # Calculate the total number of values in each matrix
+    total_values = boolean_comparison.size
+
+    trues_matrix = np.full(boolean_comparison.shape, True)
+    # Calculate the number of matching values (True values in the same positions)
+    matching_values = np.sum(boolean_comparison == trues_matrix)
+    # Calculate the threshold for 99.7% (3sigma) matching values
+    threshold = 0.997 * total_values
+
+    is_3sigma = matching_values >= threshold
+
+    if is_3sigma == True:
+        print("Yes, the differences with respect to the output TOA (", ism_toa_isrf + band,
+            ") are <0.01% for at least 3-sigma of the points.")
+    else:
+        print("No!, the differences with respect to the output TOA (", ism_toa_isrf + band,
+            ") are not all <0.01% for at least 3-sigma of the points.")
