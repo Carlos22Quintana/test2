2cfbf89fa1c8c19b43bcb4b8c775057ad6a8319f
Carlos22Quintana <145041995+Carlos22Quintana@users.noreply.github.com>
2023-10-10 21:02:11 +0200

    Martes 10.10 ISM hasta intentanto hasta el 3

--- ism/test/testISM.py
+++ ism/test/testISM.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 from common.io.writeToa import readToa
+from common.io.readMat import readMat
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,6 +16,14 @@ my_toa_path = r"C:\EODP\EODP_TER\EODP-TS-ISM\outputcarlos"
 luss_toa_path = r"C:\EODP\EODP_TER\EODP-TS-ISM\output"
 
 ism_toa_isrf = 'ism_toa_isrf_'
+ism_toa_optical = 'ism_toa_optical_'
+Hdiff = 'Hdiff_'
+Hdefoc = 'Hdefoc_'
+Hwfe = 'Hwfe_'
+Hdet = 'Hdet_'
+Hsmear = 'Hsmear_'
+Hmotion = 'Hmotion_'
+Hsys = 'Hsys_'
 
 for band in bands:
 
@@ -33,13 +42,12 @@ for band in bands:
 
     # Calculate the total number of values in each matrix
     total_values = boolean_comparison.size
-
     trues_matrix = np.full(boolean_comparison.shape, True)
     # Calculate the number of matching values (True values in the same positions)
     matching_values = np.sum(boolean_comparison == trues_matrix)
     # Calculate the threshold for 99.7% (3sigma) matching values
     threshold = 0.997 * total_values
-
+    # Apply threshold to the matching values
     is_3sigma = matching_values >= threshold
 
     if is_3sigma == True:
@@ -48,3 +56,52 @@ for band in bands:
     else:
         print("No!, the differences with respect to the output TOA (", ism_toa_isrf + band,
             ") are not all <0.01% for at least 3-sigma of the points.")
+
+    # Check for all bands that the differences with respect to the output TOA (ism_toa_optical) are <0.01%
+    # for at least 3-sigma of the points.
+
+    # 1. Read LUSS
+    luss_toa = readToa(luss_toa_path, ism_toa_optical + band + '.nc')
+
+    # 2. Read your outputs
+    my_toa = readToa(my_toa_path, ism_toa_optical + band + '.nc')
+
+    # 3. Compare
+    result = my_toa - luss_toa
+    porcentaje = result / my_toa * 100
+    df = pd.DataFrame(porcentaje)
+    porcentaje_df = df.fillna(0)  # The division per zero gives some annoying NaN values
+    boolean_comparison = np.array(porcentaje_df < 0.01)
+
+    # Calculate the total number of values in each matrix
+    total_values = boolean_comparison.size
+    trues_matrix = np.full(boolean_comparison.shape, True)
+    # Calculate the number of matching values (True values in the same positions)
+    matching_values = np.sum(boolean_comparison == trues_matrix)
+    # Calculate the threshold for 99.7% (3sigma) matching values
+    threshold = 0.997 * total_values
+    # Apply threshold to the matching values
+    is_3sigma = matching_values >= threshold
+
+    if is_3sigma == True:
+        print("Yes, the differences with respect to the output TOA (", ism_toa_optical + band,
+                  ") are <0.01% for at least 3-sigma of the points.")
+    else:
+        print("No!, the differences with respect to the output TOA (", ism_toa_optical + band,
+                  ") are not all <0.01% for at least 3-sigma of the points.")
+
+    # What is the radiance to irradiance conversion factor for each band. What are the units of the TOA at this stage.
+
+    # Plot for all bands the System MTF across and along track (for the central pixels). Report the MTF at
+    # the Nyquist frequency. Explain whether this is a decent or mediocre value and why
+
+    # Read your outputs
+    my_toa = readMat(luss_toa_path, Hdiff + band + '.nc')
+
+    nlines_ALT = my_toa.shape[0]
+    ALT_central_line = int(nlines_ALT / 2)
+    nlines_ACT = my_toa.shape[1]
+    ALC_central_line = int(nlines_ALT / 2)
+
+
+
